a16dbf153df5e75b943a9319eed5e97cc4792a52
thang <thanghn2503@gamil.com>
2022-01-22 10:32:57 +0700

    ud

--- src/model/product.js
+++ src/model/product.js
@@ -3,9 +3,14 @@ const mongoose = require('../connectToDB.js');
 const Schema = mongoose.Schema;
 const Product = new Schema({
     name: {type: String, default: '', required: true},
-    description:{type: String, default: '', required: true},
+    shortDescription:{type: String, default: '', required: true},
     price: {type: String,default: '0', required: true},
     discount: {type: String, default: '0'},
+    img :{
+        data: Buffer,
+        contentType: String,
+        src:String,
+    }
 });
 
 module.exports = mongoose.model('Product',Product);
