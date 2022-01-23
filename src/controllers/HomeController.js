ec00d7c2a763fed2b4de786368dcc6f127415920
thang <thanghn2503@gamil.com>
2022-01-23 09:35:17 +0700

    ud

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,10 +1,20 @@
 const Product = require('../model/product.js');
 const {multiObject} = require('../convertToObject.js');
 const {object} = require('../convertToObject.js');
+const User = require('../model/user.js');
 
 class HomeController {
     home(req, res, next) {
-        res.render('home');
+        User.findById({_id:'61ecb7cd767e5e0390c60b8a'}, function(err,user){
+            if(user){
+                res.render('home', {
+                    user: user
+                })
+            }
+            else {
+                res.send('ko tim thay');
+            }
+        })
     }
 }
 
