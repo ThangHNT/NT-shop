2fd02f0044b8e7a2d6c13e5f96203fad183a33d6
thang <thanghn2503@gamil.com>
2022-01-23 09:27:27 +0700

    ud

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -4,15 +4,9 @@ const {object} = require('../convertToObject.js');
 
 class HomeController {
     home(req, res, next) {
-        res.json(req.user);
-        // Product.find()
-        //     .then((products) => {
-        //         res.render('home', {
-        //             products :multiObject(products),
-        //             user: req.user,
-        //         })
-        //     })
-        //     .catch(next);
+        res.render('home', {
+            user: req.user
+        })
     }
 }
 
