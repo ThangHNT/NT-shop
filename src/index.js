9b82394bfd7dd843038fad048a7fc9e62a337e0d
thang <thanghn2503@gamil.com>
2022-02-04 20:45:41 +0700

    ud

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ app.engine('.hbs',handlebars({extname: '.hbs'}));
 app.set('view engine','.hbs');
 app.set('views', path.join(__dirname,'views'));
 app.use(express.static(path.join(__dirname,'public')));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({extended: true, limit: '50mb'}));
 
 
 fbauthentication(app);
--- src/routes/user.js
+++ src/routes/user.js
@@ -5,5 +5,6 @@ const userController = require('../controllers/UserController.js');
 router.get('/login',userController.login);
 router.get('/myAccount', userController.myAccount);
 router.post('/updateProfile', userController.updateProfile);
+router.get('/json/avatar',userController.getJSON);
 
 module.exports = router;
