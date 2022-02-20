48644f58ffcb3d053ce5c9d74c368bf6967c2471
thang <thanghn2503@gamil.com>
2022-02-20 08:47:19 +0700

    ud

--- src/model/product.js
+++ src/model/product.js
@@ -4,13 +4,18 @@ const Schema = mongoose.Schema;
 const Product = new Schema({
     name: {type: String, default: '', required: true},
     shortDescription:{type: String, default: '', required: true},
+    longDescription:[{title:String,content:String}],
     price: {type: String,default: '0', required: true},
     discount: {type: String, default: '0'},
-    img :{
-        data: Buffer,
-        contentType: String,
-        src:String,
-    }
+    totalAmount: {type: String, default: '', required: true},
+    deliveryFrom: {type: String, default: '', required: true},
+    brand: {type: String, default: '', required: true},
+    sold: {type: String, default: ''},
+    available: {type: String, default: ''},
+    genus:[{name:String,amount:String}],
+    detail:[{title:String,content:String,default:''}],
+    img: [{type: String, default: ''}],
+    img_base64 :[{data: Buffer,contentType: String,src:String,}]
 });
 
 module.exports = mongoose.model('Product',Product);
