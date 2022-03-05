6c2fb556c6ead84f60a4eef4109ac3a790d29104
thang <thanghn2503@gamil.com>
2022-03-05 23:52:45 +0700

    ud

--- src/controllers/ShopController.js
+++ src/controllers/ShopController.js
@@ -22,34 +22,41 @@ class ShopController {
         // const provider = req.user.provider;
         // var id = req.user.id;
         User.findOne({id: '1384771445288690'}, function(err, user){
-            // User.findOne({id: id, authType: provider}, function(err, user){
-                let unit = req.body.discount.unit;
-                let priceAfterDiscount = 0, price = Number(req.body.price);
-                if(unit == '%'){
-                    priceAfterDiscount = Math.floor(price - (price*req.body.discount.amount)/100);
+        // User.findOne({id: id, authType: provider}, function(err, user){
+            let unit = req.body.discount.unit;
+            let priceAfterDiscount = 0, price = Number(req.body.price);
+            if(unit == '%'){
+                priceAfterDiscount = Math.floor(price - (price*req.body.discount.amount)/100);
+            }
+            else {
+                priceAfterDiscount = Math.floor(Number(req.body.price) - req.body.discount.amount);
+            }
+            const product = new Product;
+            product.name = req.body.name;
+            product.category = req.body.productCategory;
+            product.originPrice = req.body.price == '0' ? '1' : req.body.price;
+            product.priceAfterDiscount = priceAfterDiscount;
+            product.description = req.body.description;
+            product.totalAmount = req.body.totalAmount == '0' ? '1' : req.body.totalAmount;
+            product.madeIn = req.body.madeIn;
+            product.deliveryFrom = req.body.deliveryFrom;
+            product.discount.amount = req.body.discount.amount;
+            product.discount.unit = req.body.discount.unit;
+            // product.img_base64 = req.body.imgs;
+            product.imgs = req.body.imgs;
+            product.avatar = req.body.imgs[0];
+            product.introduction = req.body.introduction;
+            product.owner = user.shop;
+            product.brand = req.body.brand;
+            product.available = Number(req.body.totalAmount);
+            product.sold = 0;
+            product.save();
+            Shop.findById({_id:user.shop},function(err, shop){
+                if(shop){
+                    shop.products.push(product._id);
+                    shop.save();
                 }
-                else {
-                    priceAfterDiscount = Math.floor(Number(req.body.price) - req.body.discount.amount);
-                }
-                const product = new Product;
-                product.name = req.body.name;
-                product.category = req.body.productCategory;
-                product.originPrice = req.body.price == '0' ? '1' : req.body.price;
-                product.priceAfterDiscount = priceAfterDiscount;
-                product.description = req.body.description;
-                product.totalAmount = req.body.totalAmount == '0' ? '1' : req.body.totalAmount;
-                product.madeIn = req.body.madeIn;
-                product.deliveryFrom = req.body.deliveryFrom;
-                product.discount.amount = req.body.discount.amount;
-                product.discount.unit = req.body.discount.unit;
-                product.img_base64 = req.body.imgs;
-                product.avatar = req.body.imgs[0];
-                product.introduction = req.body.introduction;
-                product.owner = user.shop;
-                product.brand = req.body.brand;
-                product.available = Number(req.body.totalAmount);
-                product.sold = 0;
-                product.save();
+            })
         })
     }
 }
