c6524ee55892cedb378179c9a916a562a3002c99
ThangHNT <thanghn2503@gmail.com>
2022-05-10 22:33:34 +0700

    cloudflare

--- src/authentication method/facebook.js
+++ src/authentication method/facebook.js
@@ -27,7 +27,7 @@ function authenticate(app) {
     // ==========================
     passport.use(new FacebookStrategy({
         clientID: '610750163507271',
-        clientSecret: process.env.clientSecret,
+        clientSecret: '20360b31af2259f76484428ea92e0fd4',
         callbackURL: "https://shop-hnt.herokuapp.com/auth/facebook/callback",
         profileFields: ['id', 'displayName', 'photos', 'email']
     },
--- src/authentication method/google.js
+++ src/authentication method/google.js
@@ -26,7 +26,7 @@ function ggAthentication(app) {
 
     passport.use(new GoogleStrategy({
         clientID: '604856952366-efq4pjustjjib5chvsmcvofvcoko6qbt.apps.googleusercontent.com',
-        clientSecret: process.env.clientSecretgg,
+        clientSecret: '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',
         callbackURL: "https://shop-hnt.herokuapp.com/auth/google/callback"
         },
         function(accessToken, refreshToken, profile, cb) {
